ee2da5adc85d1697c30f42ef3f66f734a05c36d9
Dominique <dominiquemeus@hotmail.com>
2023-03-13 18:15:55 +0100

    Customize template. Make it run on windows

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,8 +1,9 @@
-#include "libname/hello_world.h"
-
 #include <iostream>
 
-int main() {
+#include "libname/hello_world.h"
+
+int main(int argc, char** argp) {
+    std::cout << "test\n";
     std::cout << libname::helloWorld() << "\n";
     return 0;
 }
--- src/hello_world.cpp
+++ src/hello_world.cpp
@@ -1,3 +1,4 @@
+#include <string>
 #include "libname/hello_world.h"
 
 std::string libname::helloWorld() {
